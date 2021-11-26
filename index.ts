314a142a3aa4831370df55b9e8539faf51255de2
Alistair <alistairgarioch@gmail.com>
2021-11-26 13:19:46 +0000

    feat(index.ts): setup server and add middleware

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const Express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const PORT = process.env.SERVER_PORT || 8000;
 
 const app = Express();
 
+app.use(cors());
+app.use(morgan('short'));
+app.use(helmet());
+app.use(Express.json());
+
 app.get('/', (req, res) => res.send('server works!'));
 
-app.listen(
-  process.env.SERVER_PORT,
-  () => `🚀 server running on http://localhost:${process.env.SERVER_PORT}`
+app.listen(PORT, () =>
+  console.log(`🚀 server running on http://localhost:${PORT}`)
 );
