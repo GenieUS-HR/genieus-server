4ea5c8a459dbf453df1cd2f827bf0fd5dafcc093
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2021-11-27 11:14:25 +0000

    fix(helprequest.mock): resolve type error

--- src/mocks/helprequests.mock.ts
+++ src/mocks/helprequests.mock.ts
@@ -2,8 +2,6 @@ import HelpRequest from '../types/helprequest';
 
 const mockHelpRequest: HelpRequest = {
   id: 'string',
-  student_id: 'fea8be3e64777240',
-  tutor_id: 'fea8be3e6479812379',
   status: 'closed-complete',
   description: 'someone help me',
   time_opened: new Date('2021-10-08 04:05:06'),
@@ -15,6 +13,18 @@ const mockHelpRequest: HelpRequest = {
   language: 'JavaScript',
   code: null,
   zoom_url: 'http://it/was/there/',
+  call_length: 300,
+  favourites_only: false,
+  tutor: {
+    id: 'fea8be3e6479812379',
+    name: 'Thomas Gibson',
+    photo_url: 'https://randomuser.me/api/portraits/men/76.jpg',
+  },
+  student: {
+    id: 'fea8be3e64777240',
+    name: 'Brad Gibson',
+    photo_url: 'https://randomuser.me/api/portraits/men/75.jpg',
+  },
 };
 
 const helprequests: HelpRequest[] = [mockHelpRequest];
