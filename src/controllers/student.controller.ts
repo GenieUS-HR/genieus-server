b4486f60d595e56b6231ed9dcd4e8d82062cbf6a
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2021-11-29 15:29:46 +0000

    feat(student.controller): add updateStudent, getFavouriteTutors, setFavouriteTutor

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -2,6 +2,8 @@ import db from '../mocks/db.mock.js';
 import { Request, Response } from 'express';
 import student from '../types/student.js';
 import StudentModel from '../models/student.model.js';
+import sequelize from 'sequelize';
+import Student from '../types/student.js';
 
 export async function getAllStudents(req: Request, res: Response) {
   try {
@@ -77,10 +79,20 @@ export async function updateStudent(req: Request, res: Response) {
   try {
     const studentId = req.params.id;
     const studentReq = req.body;
-    const dbRes = await db.Student.updateStudent(studentId, studentReq);
-    res.status(202);
-    res.send(dbRes);
-    res.end();
+    const dbRes = await StudentModel.update(studentReq, {
+      where: { id: studentId },
+      returning: true,
+    });
+    if (dbRes[0] > 0) {
+      res.status(202);
+      // sequelize returns [num records, array of updated records]
+      res.send(dbRes[1][0]);
+      res.end();
+    } else {
+      res.status(404);
+      res.send(`Student id ${studentId} not found`);
+      res.end();
+    }
   } catch (error) {
     res.status(500);
     res.send(error);
@@ -91,9 +103,11 @@ export async function updateStudent(req: Request, res: Response) {
 export async function getFavouriteTutor(req: Request, res: Response) {
   try {
     const studentId = req.params.id;
-    const dbRes = await db.Student.getFavouriteTutor(studentId);
+    const { favourite_tutors } = await StudentModel.findOne({
+      where: { id: studentId },
+    });
     res.status(202);
-    res.send(dbRes);
+    res.send(favourite_tutors);
     res.end();
   } catch (error) {
     res.status(500);
@@ -105,14 +119,31 @@ export async function getFavouriteTutor(req: Request, res: Response) {
 export async function setFavouriteTutor(req: Request, res: Response) {
   try {
     const studentId = req.params.id;
-    const studentReq = req.body;
-    const dbRes = await db.Student.setFavouriteTutor(
-      studentId,
-      studentReq.tutor_id
-    );
+    const { tutor_id } = req.body;
+    const student = await StudentModel.findOne({
+      where: { id: studentId },
+    });
+    let updatedStudent: [number, Student[]] | null = null;
+    if (!student.favourite_tutors.includes(tutor_id)) {
+      updatedStudent = await StudentModel.update(
+        {
+          favourite_tutors: sequelize.fn(
+            'array_append',
+            sequelize.col('favourite_tutors'),
+            tutor_id
+          ),
+        },
+        { where: { id: studentId }, returning: true }
+      );
+    }
+    const dbRes =
+      updatedStudent && updatedStudent[0] > 0 ? updatedStudent[1][0] : student;
     res.status(202);
     res.send(dbRes);
     res.end();
+    // const dbRes = sequelizeConnection.query(
+    //   `UPDATE student SET favourite_tutors = (select array_agg(distinct e) from unnest(favourite_tutors || '{${tutor_id}}') e) WHERE  id = '${studentId}' AND NOT favourite_tutors @> '{${tutor_id}}';`
+    // );
   } catch (error) {
     res.status(500);
     res.send(error);
