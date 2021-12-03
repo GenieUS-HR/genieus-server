ec743dcc702c8829ea07d5a3851d44a74ef10555
Alistair <alistairgarioch@gmail.com>
2021-12-03 10:41:05 +0000

    feat(events): request status, send users, connect, disconnect events

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,16 +1,32 @@
 import { Server, Socket } from 'socket.io';
+import InMemorySessionStore from './session-store';
+
+const sessionStore = new InMemorySessionStore();
 
 const userHandler = (io: Server, socket: Socket) => {
+  const userID = socket.data.userID;
   // join user's room
-  socket.join(socket.data.userID);
+  socket.join(userID);
+  // save connected status
+  sessionStore.saveSession(userID, { userID, connected: true });
 
-  socket.emit('user logged on', socket.data.userID);
+  // send already connected users
+  // socket.emit('connected users', users);
+
+  socket.broadcast.emit('user connected', userID);
+
+  socket.on('request status', () => {
+    const users = sessionStore.findAllSessions();
+    socket.emit('users', users);
+  });
 
   socket.on('disconnect', async () => {
-    const matchingSockets = await io.in(socket.data.userID).allSockets();
+    const matchingSockets = await io.in(userID).allSockets();
     const isDisconnected = matchingSockets.size === 0;
     if (isDisconnected) {
-      socket.broadcast.emit('user disconnected', socket.data.userID);
+      // notify clients and save disconnected status
+      socket.broadcast.emit('user disconnected', userID);
+      sessionStore.saveSession(userID, { userID, connected: false });
     }
   });
 };
